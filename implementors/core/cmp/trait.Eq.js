6d78f6cf6b4361b0535a8169865ef3cb274cffb0
Ning Sun <sunng@about.me>
2016-05-15 19:12:05 +0800

    (cargo-release) generate docs

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["handlebars"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='https://doc.rust-lang.org/regex/regex/re_unicode/struct.Regex.html' title='regex::re_unicode::Regex'>Regex</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["handlebars"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&lt;Char&gt; for <a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.char.html'>char</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&lt;<a class='enum' href='https://doc.rust-lang.org/rustc-serialize/rustc_serialize/json/enum.Json.html' title='rustc_serialize::json::Json'>Json</a>&gt; for <a class='enum' href='https://doc.rust-lang.org/rustc-serialize/rustc_serialize/json/enum.Json.html' title='rustc_serialize::json::Json'>Json</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
